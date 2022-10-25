41acc54747200478a06a7a84447035ce60e05029
Barett Jones <barettj22@gmail.com>
2022-10-24 22:24:20 -0500

    Add recipe table

---
+++ client/src/Components/Table/RecipeTable.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Table/RecipeTable.css";
+
+const RecipeTable = () => {
+    return(
+        <div>
+            recipe table
+        </div>
+    )
+}
+
+export default RecipeTable;
