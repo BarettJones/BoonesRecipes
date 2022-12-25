5dd4f3dbc63516911fc3a043f9cbccf69155b764
BarettJones <barettj22@gmail.com>
2022-12-24 22:33:53 -0600

    add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
+import RecipeItem from "./components/recipe/RecipeItem";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const recipeArray = [
+    {
+      recipeName: "chicken pot pie",
+      recipeDescription: "juicy",
+      recipeOwner: "Barett",
+    },
+    {
+      recipeName: "cookie",
+      recipeDescription: "moist",
+      recipeOwner: "Michael",
+    },
+  ];
   return (
     <div className="App">
-      Test
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RecipeItem />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
